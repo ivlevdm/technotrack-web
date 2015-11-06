1366483ce30bc8204d286601d9ebdf62671ce14f
divlev <ivlev.dm@frtk.ru>
2015-11-06 10:29:25 +0300

    Changed urls

--- src/dewil/urls.py
+++ src/dewil/urls.py
@@ -18,10 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^popular/', include('questions.urls', namespace='popular-question', app_name='question')),
-    url(r'^ask/', include('questions.urls', namespace='new-question', app_name='question')),
-    url(r'^answers/', include('questions.urls', namespace='question', app_name='question')),
-    url(r'^login/', include('login.urls', namespace='login', app_name='login')),
-    url(r'^registration/', include('login.urls', namespace='login', app_name='login')),
-    url(r'^$', include('questions.urls', namespace='new-question', app_name='question')),
+    url(r'^', include('questions.urls', namespace='question', app_name='question')),
+    url(r'^', include('login.urls', namespace='login', app_name='login')),
 ]
--- src/questions/urls.py
+++ src/questions/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^popular/', views.popular, name='popular'),
-    url(r'^ask/', views.ask, name='ask'),
-    url(r'^answers/', views.answer, name='answer'),
+    url(r'^popular/$', views.popular, name='popular'),
+    url(r'^ask/$', views.ask, name='ask'),
+    url(r'^answers/$', views.answer, name='answer'),
     url(r'^$', views.index, name='index'),
 ]
